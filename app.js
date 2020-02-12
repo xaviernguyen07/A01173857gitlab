d50b9f59219ab53bac30dad71da406f6a14798bf
xaviernguyen07 <59535701+xaviernguyen07@users.noreply.github.com>
2020-02-12 08:49:16 -0800

    making changes

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // Add DOM selectors to target input and UL movie list
 var inp = document.querySelector("input");
 var myMovieList = document.querySelector("ul");
-var h5 = document.getElementById("movieHistory");
+
+// Added movieHistory **1
+var movieHistory = document.querySelector("#movieHistoryCard");
+
+// Added myMOvies object **2
+var myMovies = {
+    
+};
+
+if(localStorage.getItem("myMovies")) {
+    myMovies = JSON.parse(localStorage.getItem("myMovies"));
+    updateMovieHistory();
+}
+
 
 // Example of a simple function that clears the input after a user types something in
 function clearInput() {
@@ -13,70 +26,68 @@ function clearMovies() {
     myMovieList.innerHTML = '';
 }
 
+function updateMovieHistory(movie) {
+
+    var myTable= `
+    <h5 class="card-title">Movie History</h5>
+    <table id="movieHistoryTable">
+        <tr>
+            <th style='width: 400px;'>Title</th>
+            <th style='width: 400px; text-align: right;'>Watched</th>
+        </tr>
+        ${
+            Object.keys(myMovies).map(function (key) {
+                return `<tr><td>${key}</td><td style='text-align: right;'>${myMovies[key]}</td></tr>`           
+                }).join("")
+        }
+    </table>
+    `;
+    movieHistory.innerHTML = myTable;
+}
+
+function keyExists(value, obj) {
+    obj = Object.keys(obj); 
+    for (var i = 0; i < obj.length; i++) {
+      if (value.toLowerCase() === obj[i].toLowerCase()) { return obj[i]; }
+    }
+    return false;
+  }
+
 // This function is executed when the user clicks [ADD MOVIE] button.
 function addMovie() {
     // Step 1: Get value of input
     var userTypedText = inp.value;
-    // Step 2: Create an empty <li></li>
-    var li = document.createElement("li"); // <li></li>
 
-    // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
-    var textToInsert = document.createTextNode(userTypedText);
+    var keyFound = keyExists(userTypedText, myMovies);
 
-    // Step 4: Insert text into li
-    // <li>Harry Potter </li>
-    li.appendChild(textToInsert);
+    if(!keyFound) {
+        // Step 2: Create an empty <li></li>
+        var li = document.createElement("li"); // <li></li>
 
-    // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
-    myMovieList.appendChild(li);
+        // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
+        var textToInsert = document.createTextNode(userTypedText);
 
+        // Step 4: Insert text into li
+        // <li>Harry Potter </li>
+        li.appendChild(textToInsert);
+
+        // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
+        myMovieList.appendChild(li);
+    }
+
+    // Insert movie into Object
+
+    if(keyFound) {
+        myMovies[keyFound] = myMovies[keyFound] + 1 || 1;
+        localStorage.setItem("myMovies", JSON.stringify(myMovies));
+    } else {
+        myMovies[userTypedText] = myMovies[userTypedText] + 1 || 1;
+        localStorage.setItem("myMovies", JSON.stringify(myMovies));
+    }
+    
     // Step 6: Call the clearInput function to clear the input field
     clearInput();
 
-    //create <table></table>
-    var tableElement = document.createElement("TABLE");
-    //append <table></table> to <h5 id ="movieHistory"></h5>
-    h5.appendChild(tableElement);
-    // create<tr></tr>
-    var tableRow = document.createElement("TR");
-    // append <table> <tr></tr> </table>
-    tableElement.appendChild(tableRow);
-    //create <th></th>
-    var tableHead = document.createElement("TH");
-    // prepare text node to insert to <th></th>
-    var firstHead = document.createTextNode("Name of movies");
-    tableHead.appendChild(firstHead);
-    // prepare text node to insert to <th></th>
-    var secondHead = document.createTextNode("Number of times watch");
-    tableHead.appendChild(secondHead);
-    // append <th> to <tr>
-    tableRow.appendChild(tableHead);
-    //create <td></td>
-    var tableData = document.createElement("TD");
-    //prepare text to insert <td> user input </td>
-    var movieName = document.createTextNode(userTypedText);
-    // append <td> user input </td> 
-    tableData.appendChild(movieName);
-    // create <tr></tr>
-    var tableRow2 = document.createElement("TR");
-    // set id <tr id="dataContent"></tr>
-    tableRow2.appendChild(tableData);
-    // append <table> <tr> <td> user input </tr> </table>
-    tableElement.appendChild(tableRow2);
-
-    var numOfWatch = document.createTextNode("0");
-    tableData.appendChild(numOfWatch);
-
-    movieHistory = {};
-    //store name of movie and number of watch to object
-    // check object
-    movieHistory["name of movies"] = userTypedText;
-    movieHistory["num of watch"] = 0;
-    // length of movieHistory. check index
-    if (userTypedText == Object.values(movieHistory).indexOf(0)){
-        movieHistory["num of watch"] += 1;
-        console.log(movieHistory["num of watch"]);
-        
-    }
-    
+    // **3 Add to myMovies Object: watched zero times
+    updateMovieHistory(userTypedText);
 }
